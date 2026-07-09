8ec8efbc138dae62e692415439bedfb3fbde6318
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-07-09 10:49:38 +0530

    Add files via upload

---
+++ str_fun.php
@@ -0,0 +1,22 @@
+<?php
+echo chr (97);
+echo "<br>";
+echo ord("b");
+echo "<br>";
+echo strtolower("Atmiya UniY");
+echo "<br>";
+echo strtoupper("atmiya");
+echo "<br>";
+echo strlen("atmiya");
+echo "<br>";
+$t="atmiya university";
+echo strlen($t);
+echo "<br>";
+echo md5("khushi");
+echo "<br>";
+
+$a=explode(" ", "this is testfile");
+print_r($a);
+echo "<br>";
+
+?>
