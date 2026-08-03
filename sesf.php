77b7fda3290d05e0a32992fdaa02d0505edc0731
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-08-03 11:38:34 +0530

    Add files via upload

---
+++ sesf.php
@@ -0,0 +1,8 @@
+<form action="sesstion.php" method="POST">
+username:<input type="text" name="username">
+<br>
+password:<input type="text" name="pwd">
+<br>
+<input type="submit" name="submit">
+
+</form>
