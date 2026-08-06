fe0694e5517e1f19bbfbe0f78bc1b755171a9e98
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-08-06 11:36:05 +0530

    Add files via upload

---
+++ ses2.php
@@ -0,0 +1,11 @@
+<form action="arrayin_cookie.php" method="POST">
+name:<input type="text" name="name">
+<br>
+university:<input type="text" name="university">
+<br>
+
+city:<input type="text" name="city">
+<br>
+<input type="submit" name="submit">
+
+</form>
